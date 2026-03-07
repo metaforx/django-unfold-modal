f3439d9b54b5c349422f899f8bb641944fc3e620
Marc Widmer <marc@pbi.io>
2026-03-07 19:32:53 +0100

    feat: add UNFOLD_MODAL_SHOW_ADD_IN_POPUP setting (T22)
    Override unfold/helpers/add_link.html to suppress the header Add button
    in popup contexts when UNFOLD_MODAL_SHOW_ADD_IN_POPUP=False. Default is
    True (no behaviour change). Template-only change; no JS. 104 tests pass.

--- unfold_modal/templatetags/unfold_modal_tags.py
+++ unfold_modal/templatetags/unfold_modal_tags.py
@@ -1,10 +1,17 @@
-"""Template tags for unfold-modal CMS integration."""
+"""Template tags for unfold-modal."""
 
 from django import template
 
+from unfold_modal.apps import get_setting
 from unfold_modal.utils import get_cms_modal_head_html
 
 register = template.Library()
+
+
+@register.simple_tag
+def unfold_modal_show_add_in_popup():
+    """Return the UNFOLD_MODAL_SHOW_ADD_IN_POPUP setting value."""
+    return get_setting("UNFOLD_MODAL_SHOW_ADD_IN_POPUP")
 
 
 @register.simple_tag
