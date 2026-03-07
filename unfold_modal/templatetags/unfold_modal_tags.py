e51461c2387cd90921f057746b84450cda98f19d
Marc Widmer <marc@pbi.io>
2026-03-07 20:53:20 +0100

    fix: move popup add-link suppression into django-unfold (T22a)
    The T22 template override was ignored due to INSTALLED_APPS order.
    Remove the shadowed template and unused template tag from unfold_modal.
    The show_add_in_popup tag and conditional now live in django-unfold's
    add_link.html directly (committed in the django-unfold repo).

--- unfold_modal/templatetags/unfold_modal_tags.py
+++ unfold_modal/templatetags/unfold_modal_tags.py
@@ -2,16 +2,9 @@
 
 from django import template
 
-from unfold_modal.apps import get_setting
 from unfold_modal.utils import get_cms_modal_head_html
 
 register = template.Library()
-
-
-@register.simple_tag
-def unfold_modal_show_add_in_popup():
-    """Return the UNFOLD_MODAL_SHOW_ADD_IN_POPUP setting value."""
-    return get_setting("UNFOLD_MODAL_SHOW_ADD_IN_POPUP")
 
 
 @register.simple_tag
